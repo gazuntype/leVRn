f1d62eeb803cb4e02896e6689f18f283463eef90
Olutade Ajiboye <mrbigheaddd@gmail.com>
2016-12-07 11:41:59 +0100

    fix: hand colliders not moving
    Transforms of the index finger hand collider was added as target transform to the NetworkTransformChild components on the GearVRCameraRig.

--- Assets/Levrn/Scripts/Menu/MessageBoard.cs
+++ Assets/Levrn/Scripts/Menu/MessageBoard.cs
@@ -4,12 +4,11 @@ using UnityEngine.UI;
 
 
 public class MessageBoard : MonoBehaviour {
-	public GameObject[] log;
-	public Transform dashboard;
-	int position;
+	public Text[] log;
+	int index;
 	void Start()
 	{
-		position = 3;
+		index = 0;
 	}
 
 	void Update()
@@ -21,36 +20,8 @@ public class MessageBoard : MonoBehaviour {
 	{
 		if (MenuButtonClick.buttonPressed)
 		{
-			GameObject buttonClicked;
-			switch (MenuButtonClick.button.name)
-			{
-				case "moveForward":
-					buttonClicked = (GameObject)Instantiate(log[0], dashboard.position, Quaternion.identity);
-					buttonClicked.transform.SetParent(dashboard);
-					buttonClicked.transform.position = new Vector3(0, 0.15f * position, 0);
-					break;
-				case "moveLeft":
-					buttonClicked = (GameObject)Instantiate(log[1], dashboard.position, Quaternion.identity);
-					buttonClicked.transform.SetParent(dashboard);
-					buttonClicked.transform.position = new Vector3(0, 0.15f * position, 0);
-					break;
-				case "moveRight":
-					buttonClicked = (GameObject)Instantiate(log[2], dashboard.position, Quaternion.identity);
-					buttonClicked.transform.SetParent(dashboard);
-					buttonClicked.transform.position = new Vector3(0, 0.15f * position, 0);
-					break;
-				case "jump":
-					buttonClicked = (GameObject)Instantiate(log[3], dashboard.position, Quaternion.identity);
-					buttonClicked.transform.SetParent(dashboard);
-					buttonClicked.transform.position = new Vector3(0, 0.15f * position, 0);
-					break;
-				case "reset":
-					buttonClicked = (GameObject)Instantiate(log[4], dashboard.position, Quaternion.identity);
-					buttonClicked.transform.SetParent(dashboard);
-					buttonClicked.transform.position = new Vector3(0, 0.15f * position, 0);
-					break;
-			}
-			position -= 1;
+			log[index].text = MenuButtonClick.button.name + "()";
+			index += 1;
 			MenuButtonClick.buttonPressed = false;
 		}
 	}
