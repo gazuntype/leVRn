2d948dffb15530f9644a0f13babc17c0f34bbb79
Olutade Ajiboye <mrbigheaddd@gmail.com>
2017-05-05 14:25:38 +0100

    feat: set up explore
    Explore mode has been set up and instructions are given on how to move
    and what the mode is about.

--- Assets/gear-vr-leap/Scripts/NetworkControl.cs
+++ Assets/gear-vr-leap/Scripts/NetworkControl.cs
@@ -1,9 +1,13 @@
 ﻿using UnityEngine;
 using UnityEngine.Networking;
+using UnityEngine.UI;
 using System.Collections;
 
 public class NetworkControl : MonoBehaviour
 {
+
+	#region Variable Declaration
+	[Header("Network Setup")]
 	public GameObject startServer;
 	public GameObject startClient;
 	public GameObject createRoom;
@@ -13,6 +17,23 @@ public class NetworkControl : MonoBehaviour
 	public GameObject explore;
 	public GameObject back;
 
+	[Header("Dynamic Texts")]
+	public Text title;
+	public Text subTitle;
+	public Text leftTitle;
+	public Text leftInformation;
+	public Text rightTitle;
+	public Text rightInformation;
+
+	[Header("Canvas")]
+	public GameObject curvedCanvas;
+
+
+	[HideInInspector]
+	public static bool isExploring = false;
+	#endregion
+
+	#region Network Set-Up
 	NetworkManager manager;
 	// Use this for initialization
 	void Start()
@@ -63,11 +84,6 @@ public class NetworkControl : MonoBehaviour
 		back.SetActive(false);
 	}
 
-	public void Explore()
-	{
-
-	}
-
 	public void CreateRoom()
 	{
 		if (!NetworkClient.active && !NetworkServer.active && manager.matchMaker == null)
@@ -83,4 +99,30 @@ public class NetworkControl : MonoBehaviour
 			manager.StartClient();
 		}
 	}
+	#endregion
+
+	#region Explore
+	public void Explore()
+	{
+		startServer.SetActive(false);
+		startClient.SetActive(false);
+		explore.SetActive(false);
+		subTitle.text = "Go out! Explore.";
+		leftTitle.text = "Explore!";
+		leftInformation.text = "Feel free to explore the world \n" +
+			"There are a lot of things to learn \n" +
+			"Visit the other tents and enjoy \n \n" +
+			"To connect your Leap Motion controller which allows you partake in certain learning exercises, come back to this tent.";
+		rightTitle.text = "Navigation Instructions";
+		rightInformation.text = "To move around, locate a teleportation pad. They are scattered around the area \n" +
+			"Look in the direction of a pad and when it glows up, click your headset to teleport to where the pad is. \n" +
+			"Go out and explore the world of learning!";
+	}
+
+	public void Begin()
+	{
+		isExploring = true;
+		curvedCanvas.SetActive(false);
+	}
+	#endregion
 }
