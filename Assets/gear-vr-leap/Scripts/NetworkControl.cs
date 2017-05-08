73fed3dbddaa9c64ac3bef618ad782713aea5148
Olutade Ajiboye <mrbigheaddd@gmail.com>
2017-05-09 00:41:43 +0100

    feat: edited NetworkControl script
    NetworkControl script was edited to be more organised

--- Assets/gear-vr-leap/Scripts/NetworkControl.cs
+++ Assets/gear-vr-leap/Scripts/NetworkControl.cs
@@ -32,6 +32,30 @@ public class NetworkControl : MonoBehaviour
 
 	[HideInInspector]
 	public static bool isExploring = false;
+
+	private string[,] instructionText = new string[4,2];
+	private string[,] titleText = new string[2, 2];
+	#endregion
+
+	#region TextSetUp
+	void Awake()
+	{
+		titleText[0, 0] = "leVRn";
+		titleText[1, 0] = "set up your leap motion";
+		titleText[0, 1] = "leVRn";
+		titleText[1, 1] = "Go out! Explore.";
+
+		instructionText[0, 0] = "Mobile Setup";
+		instructionText[1, 0] = "PC SETUP";
+		instructionText[2, 0] = "Select Server\nRead out the displayed IP Address\nCreate Room\n\nps: selections are made by gazing at the button to be selected and clicking.";
+		instructionText[3, 0] = "Select client\nFill in the IP Address of the server (mobile phone)\nAfter a room has been created by the server, select join room\n\nps: selections are made by gazing at the button to be selected and clicking.";
+
+		instructionText[0, 1] = "Explore!";
+		instructionText[1, 1] = "Navigation Instructions";
+		instructionText[2, 1] = "Feel free to explore the world \n There are a lot of things to learn \n Visit the other tents and enjoy \n \n To connect your Leap Motion controller which allows you partake in certain learning exercises, come back to this tent.";
+		instructionText[3,1] = "To move around, locate a teleportation pad. They are scattered around the area \n Look in the direction of a pad and when it glows up, click your headset to teleport to where the pad is. \n Go out and explore the world of learning!";
+	}
+
 	#endregion
 
 	#region Network Set-Up
@@ -109,21 +133,21 @@ public class NetworkControl : MonoBehaviour
 		startServer.SetActive(false);
 		startClient.SetActive(false);
 		explore.SetActive(false);
-		subTitle.text = "Go out! Explore.";
-		leftTitle.text = "Explore!";
-		leftInformation.text = "Feel free to explore the world \n" +
-			"There are a lot of things to learn \n" +
-			"Visit the other tents and enjoy \n \n" +
-			"To connect your Leap Motion controller which allows you partake in certain learning exercises, come back to this tent.";
-		rightTitle.text = "Navigation Instructions";
-		rightInformation.text = "To move around, locate a teleportation pad. They are scattered around the area \n" +
-			"Look in the direction of a pad and when it glows up, click your headset to teleport to where the pad is. \n" +
-			"Go out and explore the world of learning!";
+		subTitle.text = titleText[1, 1];
+		leftTitle.text = instructionText[0, 1];
+		leftInformation.text = instructionText[2, 1];
+		rightTitle.text = instructionText[1, 1];
+		rightInformation.text = instructionText[3, 1];
 	}
 
 	public void Begin()
 	{
 		isExploring = true;
+		subTitle.text = titleText[1, 0];
+		leftTitle.text = instructionText[0, 0];
+		leftInformation.text = instructionText[2, 0];
+		rightTitle.text = instructionText[1, 0];
+		rightInformation.text = instructionText[3, 0];
 		curvedCanvas.SetActive(false);
 		titleCanvas.SetActive(true);
 	}
