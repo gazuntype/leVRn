71e59211ab086fa5bc6e56a19b246e6056e2a664
Olutade Ajiboye <mrbigheaddd@gmail.com>
2017-05-11 16:51:37 +0100

    fix: start position of player prefab in online scene

---
+++ Assets/gear-vr-leap/Scripts/StartPosition.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartPosition : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		transform.position = new Vector3(-21.6f, 2f, -21.9f);
+		transform.rotation = Quaternion.identity;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
