3419f257cbea30cfd3776b4b7bed238fcdc0d182
Olutade Ajiboye <mrbigheaddd@gmail.com>
2017-07-12 14:46:14 +0100

    feat: set up selecting menu items

--- Assets/Levrn/Scripts/Menu/MenuButtonClick.cs
+++ Assets/Levrn/Scripts/Menu/MenuButtonClick.cs
@@ -3,11 +3,15 @@ using System.Collections;
 
 public class MenuButtonClick : MonoBehaviour
 {
+	
 	public static bool buttonPressed = false;
 	public static GameObject button;
+
+	TitleScreen titleScreen;
+	bool front;
 	// Use this for initialization
 	void Start () {
-	
+		titleScreen = GameObject.Find("RigController").GetComponent<TitleScreen>();
 	}
 	
 	// Update is called once per frame
@@ -16,7 +20,40 @@ public class MenuButtonClick : MonoBehaviour
 	}
 
 	void OnTriggerEnter(Collider other){
-		buttonPressed = true;
-		button = other.gameObject;
+		if (other.tag == "button")
+		{
+			front = CheckIfObjectIsInFront(other.transform, transform);
+			if (front)
+			{
+				ButtonClicked(other.name);
+			}
+		}
 	}
+
+	bool CheckIfObjectIsInFront(Transform buttonClicked, Transform finger)
+	{
+		bool inFront = false;
+		Vector3 displacement = finger.position - buttonClicked.position;
+		float dotProduct = Vector3.Dot(displacement, buttonClicked.forward);
+		if (dotProduct > 0)
+		{
+			inFront = true;
+		}
+		else if (dotProduct < 0)
+		{
+			inFront = false;
+		}
+		return inFront;
+	}
+
+	void ButtonClicked(string buttonName)
+	{
+		switch (buttonName)
+		{
+			case "Play":
+				titleScreen.ClickedPlay();
+				break;
+		}
+	}
+
 }
--- Assets/Levrn/Scripts/UI/TitleScreen.cs
+++ Assets/Levrn/Scripts/UI/TitleScreen.cs
@@ -4,13 +4,35 @@ using UnityEngine;
 
 public class TitleScreen : MonoBehaviour {
 
+	public GameObject title;
+	public GameObject menuScreen;
+
+	UIStates state;
+
 	// Use this for initialization
 	void Start () {
-		
+		state = UIStates.title;
+		StartCoroutine(FadeWelcomeScreen());
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+	IEnumerator FadeWelcomeScreen()
+	{
+		yield return new WaitForSeconds(3);
+		title.SetActive(false);
+		menuScreen.SetActive(true);
+		state = UIStates.menu;
+	}
+
+	public void ClickedPlay()
+	{
+		menuScreen.SetActive(false);
+		state = UIStates.level;
+	}
+
+	enum UIStates { title, menu, level, help, sublevel }
 }
