759ba42a836512e65ccbae5f633bdc1f46e4cb9a
Olutade Ajiboye <mrbigheaddd@gmail.com>
2017-07-16 23:52:41 +0100

    feat: canvas faces user always

---
+++ Assets/Levrn/Scripts/UI/CanvasRotation.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanvasRotation : MonoBehaviour
+{
+	public GameObject player;
+	// Use this for initialization
+	void Start()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		Vector3 mirrorTransform;
+		mirrorTransform = transform.position + (transform.position - player.transform.position);
+		transform.LookAt(mirrorTransform);
+	}
+}
